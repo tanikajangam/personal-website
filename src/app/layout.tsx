83d586e2c152ecaa5b30a16f6479503ce56c60e6
“Tanika <tanikajangam03@gmail.com>
2025-01-13 22:01:38 -0500

    updating typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,13 @@
+// Import Open Sans from next/font/google
+import { Open_Sans } from "next/font/google";
+
+// Configure Open Sans with specific subsets and weights
+const openSans = Open_Sans({
+  subsets: ["latin"], // Define character subsets
+  weight: ["400", "600", "700"], // Specify font weights
+});
+
+
 export const metadata = {
   title: "Tanika's Portfolio",
   description: "Welcome to my website",
@@ -10,9 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={openSans.className}>
         {children}
       </body>
-    </html>
+    </html >
   );
 }
